1e01c66430fead3f8f3fdd77f90c2cba8af78b2b
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2019-04-06 14:20:28 -0400

    Implemeted RPS strategy game with buttons and a random strategy in an html page. Next steps are to include to the tfjs learning code.

---
+++ rps.js
@@ -0,0 +1,38 @@
+function run(){
+	var myMove;
+	var opMove;
+
+	myMove = document.getElementById('pText').innerHTML.charAt(0);
+
+	moves = ["ROCK", "PAPER", "SCISSORS"];
+
+	var opMoveIdx = Math.floor((Math.random() * 3) + 0);
+	opMove = moves[opMoveIdx]
+
+	document.getElementById('oText').innerHTML = opMove;
+
+	opMove = opMove.charAt(0);
+
+	var reward = 0;
+
+	if(myMove != opMove){
+		if(myMove == "R" && opMove == "P" ){
+			reward = 10;
+		}
+		if(myMove == "R" && opMove == "S" ){
+			reward = -10;
+		}
+		if(myMove == "P" && opMove == "S" ){
+			reward = 10;
+		}
+		if(myMove == "P" && opMove == "R" ){
+			reward = -10;
+		}
+		if(myMove == "S" && opMove == "R" ){
+			reward = 10;
+		}
+		if(myMove == "S" && opMove == "P" ){
+			reward = -10;
+		}
+	}
+}
