d8ba82e2b7b43a95f02fcf9797c0f657a286e4c1
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2019-04-06 23:34:23 -0400

    added tf as lib to the html page. Also converted the input into 1-hot so that the outputs are more stable. Need to adjust the reward value.

--- rps.js
+++ rps.js
@@ -1,3 +1,10 @@
+//create model class
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 3, inputShape: [3], activation: 'relu'}));
+model.add(tf.layers.dense({units: 3}));
+
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
 function run(){
 	var myMove;
 	var opMove;
@@ -5,9 +12,16 @@ function run(){
 	myMove = document.getElementById('pText').innerHTML.charAt(0);
 
 	moves = ["ROCK", "PAPER", "SCISSORS"];
-
+	
+	//choose from model
+	var intMove = convertToInt(myMove);
+	const xs = tf.tensor2d(intMove, [1,3]);
+	var probs = model.predict(xs).arraySync()[0];
+	//show probs in a graph
+	//choose opMoveIdx using greedy epsilon
+	console.log(tf.softmax(probs).arraySync());
 	var opMoveIdx = Math.floor((Math.random() * 3) + 0);
-	opMove = moves[opMoveIdx]
+	opMove = moves[opMoveIdx];
 
 	document.getElementById('oText').innerHTML = opMove;
 
@@ -28,4 +42,19 @@ function run(){
 	}else{
 		document.getElementById('result').innerHTML = "Player Ties";
 	}
+
+	//update model
+	console.log(probs)
+	probs[opMoveIdx] = probs[opMoveIdx] + reward;
+	console.log(probs)
+	const ys = tf.tensor2d(probs, [1, 3])
+	model.fit(xs, ys);
+	console.log("fit model")
+}
+
+function convertToInt(move){
+	if(move=="R") return [1, 0, 0];
+	if(move=="P") return [0, 1, 0];
+	if(move=="S") return [0, 0, 1];
+	//throw error
 }
