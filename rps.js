cccdb1f1c542e46e2a7a24137c7ef894a91c37cf
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2019-04-17 18:03:36 -0400

    Added in triple plots in triple columns and changed rps so that there are buttons to reward positive and nagative behavior.

--- rps.js
+++ rps.js
@@ -3,38 +3,68 @@ const model = tf.sequential();
 model.add(tf.layers.dense({units: 3, inputShape: [3], useBias: false, kernelInitializer: 'heNormal'}));
 model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-function run(){
+moves = ["ROCK", "PAPER", "SCISSORS"];
+var xs;
+var logits;
+
+function chooseMove(){
+	
 	var myMove;
 	var opMove;
+	var training = true;
 
 	myMove = document.getElementById('pText').innerHTML.charAt(0);
 
-	moves = ["ROCK", "PAPER", "SCISSORS"];
-	
-	//choose from model
-	var intMove = convertToInt(myMove);
-	const xs = tf.tensor2d(intMove, [1,3]);
-	var logits = model.predict(xs).arraySync()[0];
-	//show probs in a graph
-	var probs = tf.softmax(logits).arraySync();
-	var data = [
-		{
-		x: moves,
-		y: probs,
-		type:'bar'
-		}
-	];
-	Plotly.newPlot('myDiv', data)
-	//choose opMoveIdx using greedy epsilon
-	var opMoveIdx = Math.floor((Math.random() * 3) + 0);
-	opMove = moves[opMoveIdx];
+	if(!training){
+		//choose from model
+		var intMove = convertToInt(myMove);
+		xs = tf.tensor2d(intMove, [1,3]);
+		logits = model.predict(xs).arraySync()[0];
+	}else{
+		//choose randomly 
+		var opMoveIdx = Math.floor((Math.random() * 3) + 0);
+		opMove = moves[opMoveIdx];
+	}
 
 	document.getElementById('oText').innerHTML = opMove;
+}
 
-	opMove = opMove.charAt(0);
+function plotProbs(){
+	var divs = ['div1', 'div2', 'div3']
+	var probs;
+	var data;
+	for(var i=0;i<3;i++){
+		xs = tf.tensor2d(convertToInt(moves[i].charAt(0)), [1, 3]);
+		logits = model.predict(xs).arraySync()[0];
+		probs = tf.softmax(logits).arraySync();
+		data = [
+			{
+				x:moves,
+				y:probs,
+				type:'bar'
+			}
+		];
 
+		var layout = {
+			title: 'what should i play against ' + moves[i] + '?',
+			width: 450,
+			height: 300
+
+		};
+		Plotly.newPlot(divs[i], data, layout);
+	}
+}
+
+function train(rew){
+	opMoveFull = document.getElementById('oText').innerHTML;
+	opMove = opMoveFull.charAt(0);
+	myMove = document.getElementById('pText').innerHTML.charAt(0);
+	opMoveIdx = moves.indexOf(opMoveFull);
 	var reward = 0;
 
+	var intMove = convertToInt(myMove);
+	xs = tf.tensor2d(intMove, [1,3]);
+	logits = model.predict(xs).arraySync()[0];
 	if(myMove != opMove){
 		if(myMove == "R" && opMove == "P" ||  myMove == "P" && opMove == "S"|| myMove == "S" && opMove == "R"  ){
 			reward = 10;
@@ -54,6 +84,10 @@ function run(){
 	logits[opMoveIdx] = logits[opMoveIdx] + reward;
 	const ys = tf.tensor2d(logits, [1, 3])
 	model.fit(xs, ys);
+	
+	//plot the probabilities
+	plotProbs();
+
 }
 
 function convertToInt(move){
