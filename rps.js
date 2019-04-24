810ebbb8f7f16144726e54444fc997c3c9b50a15
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2019-04-24 12:21:57 -0400

    Final updates --> removed dependancy on the html document and instead am passing the moves back and forth. Added function to calculate the max index during evaluation. Added text to show who's move is which move. Plotted the probabilties from the initialization.

--- rps.js
+++ rps.js
@@ -1,31 +1,55 @@
-//create model class
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 3, inputShape: [3], useBias: false, kernelInitializer: 'heNormal'}));
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
+//definitions
 moves = ["ROCK", "PAPER", "SCISSORS"];
+var myMove;
+var opMove;
+var opMoveIdx;
+const model = tf.sequential();
 
-function chooseMove(){
+/**
+ *Initialize the model parameters and plot the initial probabilities.
+ */
+function init(){
+	//create model class
+	model.add(tf.layers.dense({units: 3, inputShape: [3], useBias: false, kernelInitializer: 'heNormal'}));
+	model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+	plotProbs();
+}
+
+/**
+ * Choose an agent's move based on the phase.
+ * Training Phase: Choose actions randomly
+ * Evaluation Phase: Choose actions based on argmax probability.
+ *
+ * Params: My Move
+ * Return: Agent Move
+ */
+
+function chooseMove(move){
 	
-	var myMove;
-	var opMove;
 	var training = true;
 
-	myMove = document.getElementById('pText').innerHTML.charAt(0);
+	myMove = move;
+	var phase = document.getElementsByName('phase')
 
-	if(!training){
+	if(phase[1].checked){
 		//choose from model
 		var intMove = convertToInt(myMove);
 		var xs = tf.tensor2d(intMove, [1,3]);
 		var logits = model.predict(xs).arraySync()[0];
+		//get max
+		opMove = moves[getMaxIndex(logits)];
 	}else{
 		//choose randomly 
-		var opMoveIdx = Math.floor((Math.random() * 3) + 0);
+	 	opMoveIdx = Math.floor((Math.random() * 3) + 0);
 		opMove = moves[opMoveIdx];
 	}
 
-	document.getElementById('oText').innerHTML = opMove;
+	return opMove;
 }
+
+/**
+ * Plot the probabilities of the choosing a move for each of the my moves.
+ */
 
 function plotProbs(){
 	var divs = ['div1', 'div2', 'div3']
@@ -34,7 +58,7 @@ function plotProbs(){
 	var xs;
 	var logits;
 	for(var i=0;i<3;i++){
-		xs = tf.tensor2d(convertToInt(moves[i].charAt(0)), [1, 3]);
+		xs = tf.tensor2d(convertToInt(moves[i]), [1, 3]);
 		logits = model.predict(xs).arraySync()[0];
 		probs = tf.softmax(logits).arraySync();
 		data = [
@@ -55,27 +79,57 @@ function plotProbs(){
 	}
 }
 
+/**
+ * Trains the model based on the reward given by the user.
+ *
+ * Params: reward
+ * Return: None
+ */
 function train(reward){
-	opMoveFull = document.getElementById('oText').innerHTML;
-	opMove = opMoveFull.charAt(0);
-	myMove = document.getElementById('pText').innerHTML.charAt(0);
-	opMoveIdx = moves.indexOf(opMoveFull);
+	var phase = document.getElementsByName('phase')
+	if(phase[0].checked){
+		
+		var intMove = convertToInt(myMove);
+		var xs = tf.tensor2d(intMove, [1,3]);
+		var logits = model.predict(xs).arraySync()[0];
 
-	var intMove = convertToInt(myMove);
-	var xs = tf.tensor2d(intMove, [1,3]);
-	var logits = model.predict(xs).arraySync()[0];
-
-	//update model
-	logits[opMoveIdx] = logits[opMoveIdx] + reward;
-	const ys = tf.tensor2d(logits, [1, 3])
-	model.fit(xs, ys).then(()=>{
-		plotProbs();
-	});
+		//update model
+		logits[opMoveIdx] = logits[opMoveIdx] + reward;
+		const ys = tf.tensor2d(logits, [1, 3])
+		model.fit(xs, ys).then(()=>{
+			plotProbs();
+		});
+	}
 }
 
+/**
+ * Converts a move into a one-hot vector.
+ *
+ * Params: Move
+ * Return: One-Hot-Vector
+ */
 function convertToInt(move){
-	if(move=="R") return [1, 0, 0];
-	if(move=="P") return [0, 1, 0];
-	if(move=="S") return [0, 0, 1];
+	if(move=="ROCK") return [1, 0, 0];
+	if(move=="PAPER") return [0, 1, 0];
+	if(move=="SCISSORS") return [0, 0, 1];
 	//throw error
+}
+
+/**
+ * Choose the index of the maximum value from the array.
+ *
+ * Params: Array of values
+ * Return: Index of the max value
+ */
+function getMaxIndex(values){
+	var max=values[0];
+	var index=0;
+
+	for(var i=1;i<values.length;i++){
+		if(values[i]>max){
+			max = values[i];
+			index = i;
+		}
+	}
+	return index;
 }
